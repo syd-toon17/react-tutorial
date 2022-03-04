1b884a1bdf0ccc21983420569cde828657f22387
Sydney Wilson <syd.toon17@gmail.com>
2022-03-04 16:04:52 -0600

    Completed functionality of app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
@@ -16,7 +17,8 @@ function App() {
   return (
     <div>
      <DisplayEntries parentEntries={entries}/>
-     <AddEntryForm addNewEntryProperty={addNewEntry}/>
+     <AddEntryForm addNewEntry={addNewEntry}/>
+     <EntriesChartTracker parentEntries={entries}/>
 
     </div>
   );
